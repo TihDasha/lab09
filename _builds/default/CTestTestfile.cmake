7588d0f683c4a509c9a0557f044f4ec580f4d571
TihDasha <tihdasha06@gmail.com>
2021-08-18 03:47:54 +0300

    All files

---
+++ _builds/default/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/daria/TihDasha/workspace/projects/lab07
+# Build directory: /home/daria/TihDasha/workspace/projects/lab07/_builds/default
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/daria/TihDasha/workspace/projects/lab07/CMakeLists.txt;57;add_test;/home/daria/TihDasha/workspace/projects/lab07/CMakeLists.txt;0;")
+subdirs("third-party/gtest")
---
+++ _builds/third-party/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daria/TihDasha/workspace/projects/lab07/third-party/gtest/googletest
+# Build directory: /home/daria/TihDasha/workspace/projects/lab07/_builds/third-party/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
